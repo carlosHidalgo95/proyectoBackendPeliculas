27cd1018b231e24d58e8ac62ee237fc17b396e4b
Carlos <karucupcake@gmail.com>
2022-12-02 19:07:45 +0100

    testing railway

--- db/db.js
+++ db/db.js
@@ -2,13 +2,13 @@ const config=require('../config/config.json');
 const { Sequelize }=require('sequelize');
 
 const sequelize=new Sequelize(
-    config.development.database,
-    config.development.username,
-    config.development.password,
+    config.production.database,
+    config.production.username,
+    config.production.password,
     {
-        host:config.development.host,
-        port:config.development.port,
-        dialect:config.development.dialect
+        host:config.production.host,
+        port:config.production.port,
+        dialect:config.production.dialect
     }
 )
 module.exports=sequelize;
