97b1ea55b2cf1928b7645aea618f1ea8d276a137
Carlos <karucupcake@gmail.com>
2022-12-02 18:59:21 +0100

    testing railway

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const router = require('./router');
 
-// const PORT = 3002;
+const PORT = 3002;
 
 //SALTARSE POLITICA CORS DE GOOGLE CHROME PARA PRUEBAS LOCALES CON EL FRONTED
 app.use((req,res,next)=>{
@@ -24,7 +24,7 @@ app.use((req,res,next)=>{
 app.use(express.json());
 app.use(router);
 
-app.listen(`0.0.0.0:$PORT`, ()=>{
+app.listen(PORT,'0.0.0.0', ()=>{
     console.log(`El servidor esta up y alojado en el puerto => ${PORT}`);
 
     db.sync({force:true}).then(()=> {
