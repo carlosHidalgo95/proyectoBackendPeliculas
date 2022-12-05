7cfb44a6b7a946dc6f5c4e8d91cb0f9387d496e5
carlosHidalgo95 <karucupcake@gmail.com>
2022-12-05 02:46:55 +0100

    railway test

--- controllers/ordersControllers.js
+++ controllers/ordersControllers.js
@@ -27,7 +27,7 @@ orderController.getOrdersByUser = async (req, res) => {
                 where: { id_article: order.dataValues.id_article }
             }
         )
-        order.title=movie.dataValues.title;
+        resp[order].title=movie.dataValues.title;
         console.log("---------------MOVIE----------")
         console.log(movie);
 
